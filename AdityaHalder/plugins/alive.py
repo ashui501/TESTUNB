c3f80fbaa4c8221ce64b714e4ed26058b2a5f276
Ashui <113001130+ashui501@users.noreply.github.com>
2024-05-18 22:52:13 +0530

    Update alive.py

--- AdityaHalder/plugins/alive.py
+++ AdityaHalder/plugins/alive.py
@@ -2,7 +2,7 @@ from .. import *
 
 @app.on_message(commandx(["alive"]))
 async def alive_check(client, message):
-    await message.reply_text("**🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Dᴇᴀʀ Gᴇɴɪᴜs Mᴀsᴛᴇʀ ✨ ...**")
+    await message.reply_text("**🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Mᴀsᴛᴇʀ ✨ ...**")
 
 
 
